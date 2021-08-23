0e346bbc6f11f3507491a66195861d3e5088339e
Miguel Ramos <mframosg@eafit.edu.co>
2021-08-23 14:27:25 -0500

    initial commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.layout')
+
+@section('content')
+    <div class="container">
+
+        <div class="row justify-content-center">
+
+            <div class="col-md-8">
+
+                <div class="row">
+                    <div class="col-sm-6">
+                      <div class="card">
+                        <div class=" container-button card-body">
+                          <h5 class="card-title text-center">Activty 1</h5>
+                          <a href="#" class=" button-activity btn btn-primary btn-danger">Click here</a>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <div class="card">
+                        <div class=" container-button card-body">
+                          <h5 class="card-title text-center">Activty 2</h5>
+                          <a href="#" class=" button-activity btn btn-primary">Click here</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+@endsection
